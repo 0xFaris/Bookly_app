f6357141c4111d060d93b565e1fe21aa5cc2c7b8
0xFaris <3zands2@gmail.com>
2025-02-14 02:01:17 +0400

    refactor SplashViewBody to a StatefulWidget and implement navigation to HomeView after a delay

--- lib/Features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,28 @@
 import 'package:bookly_app/Core/utils/assets.dart';
+import 'package:bookly_app/Features/Home/presentation/view/home_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    Future.delayed(
+      const Duration(seconds: 2),
+      () {
+        Get.to(() => const HomeView(),
+            transition: Transition.fadeIn,
+            duration: const Duration(seconds: 1));
+      },
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
