ebbcc48fff213f7b4b51922b7a1b0f972ebcaa5c
0xFaris <3zands2@gmail.com>
2025-02-18 15:34:48 +0400

    add HomeViewBody and CustomAppBar widgets; update HomeView to use HomeViewBody

---
+++ lib/Features/Home/presentation/view/widgets/custom_appbar.dart
@@ -0,0 +1,32 @@
+import 'package:bookly_app/Core/utils/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+        child: Row(
+          children: [
+            Image.asset(
+              AssetsData.logo,
+              height: 26,
+            ),
+            Spacer(),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                FontAwesomeIcons.magnifyingGlass,
+                size: 30,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
