e3c957e75c39c499e2923e8b0d84f345171f1b9b
0xFaris <3zands2@gmail.com>
2025-02-20 22:00:27 +0400

    add BestSellerListViewItem widget to HomeViewBody; update styles and layout

--- lib/Features/Home/presentation/view/widgets/home_view_body.dart
+++ lib/Features/Home/presentation/view/widgets/home_view_body.dart
@@ -1,7 +1,9 @@
+import 'package:bookly_app/Core/utils/assets.dart';
 import 'package:bookly_app/Core/utils/styles.dart';
 import 'package:bookly_app/Features/Home/presentation/view/widgets/custom_appbar.dart';
 import 'package:bookly_app/Features/Home/presentation/view/widgets/features_custom_listview.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -31,13 +33,73 @@ class HomeViewBody extends StatelessWidget {
                 ),
                 Text(
                   "Best Sellers",
-                  style: Styles.titleMedium,
+                  style: Styles.textStyle18,
                 ),
               ],
             ),
-          )
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.04,
+          ),
+          BestSellerListViewItem(),
         ],
       ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: 71,
+          height: 114,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.orange,
+            image: DecorationImage(
+              image: AssetImage(AssetsData.testBanner),
+              fit: BoxFit.fill,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 20,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "The Four",
+              style: TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              "Faris",
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Text(
+                  r"14.5 $",
+                  style: Styles.textStyle18,
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.yellow,
+                ),
+                Text("4.8")
+              ],
+            )
+          ],
+        ),
+      ],
     );
   }
 }
