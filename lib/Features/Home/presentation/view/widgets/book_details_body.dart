cd4caf5a823a289fb976851b9e3a9d08ee43403f
0xFaris <3zands2@gmail.com>
2025-02-27 17:58:59 +0400

    refactor BookDetailsBody; remove unused imports for cleaner code

--- lib/Features/Home/presentation/view/widgets/book_details_body.dart
+++ lib/Features/Home/presentation/view/widgets/book_details_body.dart
@@ -1,11 +1,6 @@
-import 'package:bookly_app/Core/utils/styles.dart';
-import 'package:bookly_app/Features/Home/presentation/view/widgets/book_action.dart';
 import 'package:bookly_app/Features/Home/presentation/view/widgets/book_details_section.dart';
-import 'package:bookly_app/Features/Home/presentation/view/widgets/book_rating.dart';
 import 'package:bookly_app/Features/Home/presentation/view/widgets/custom_book_details_appbar.dart';
-import 'package:bookly_app/Features/Home/presentation/view/widgets/custom_book_image.dart';
 import 'package:bookly_app/Features/Home/presentation/view/widgets/similar_book_details_section.dart';
-import 'package:bookly_app/Features/Home/presentation/view/widgets/similar_book_list_view.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsBody extends StatelessWidget {
