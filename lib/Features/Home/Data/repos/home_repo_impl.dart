a1c55d39fa2796ef3ffb8bf3ca66bc6bb7a5d10d
0xFaris <3zands2@gmail.com>
2025-03-03 20:17:26 +0400

    implement fetchNewestBooks method in HomeRepoImpl to retrieve the latest programming books

--- lib/Features/Home/Data/repos/home_repo_impl.dart
+++ lib/Features/Home/Data/repos/home_repo_impl.dart
@@ -1,13 +1,26 @@
 import 'package:bookly_app/Core/errors/failures.dart';
+import 'package:bookly_app/Core/utils/api_service.dart';
 import 'package:bookly_app/Features/Home/Data/models/book_model/book_model.dart';
 import 'package:bookly_app/Features/Home/Data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:programming');
+
+      List<BookModel> books = [];
+      books.add(BookModel.fromJson(data));
+
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
